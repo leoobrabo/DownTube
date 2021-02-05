a621e3192f722398c66a4da5adfc3be394d0fa4f
leoobrabo <leonardomotta2010@hotmail.com>
2021-02-05 13:30:50 -0300

    interface grafica

--- app.py
+++ app.py
@@ -1,59 +1,92 @@
-# Realizar download de video unico ou playlist do youtube via Python
+import sys
+import PySimpleGUI as sg
+from pytube import YouTube
 
-import pytube  # /usr/bin/pip3 install pytube
+
+'''
+# Digite o Link do video e o local que deseja salvar o video #
+
+link = input("Digite o link do video que deseja salvar: ")
+#path = input("Digite o diretorio que deseja salvar o video:")
+yt = YouTube(link)
+
+# Mostra os detalhes do Video #
+
+print('Titulo: ', yt.title)
+print('Numero de views: ', yt.views)
+print('Tamanho do video: ', yt.length)
+print('Avaliação do video: ', yt.rating)
+
+# Usa a maior resolução #
+
+#ys = yt.streams.get_audio_only
+#ys = yt.streams.get_highest_resolution()
+
+# Começa o Download do Video #
+
+print('Baixando')
+# ys.download(path)
+print('Download Completo!')
+
+
+
 from pytube import Playlist
+pl = Playlist("https://www.youtube.com/watch?v=Edpy1szoG80&list=PL153hDY-y1E00uQtCVCVC8xJ25TYX8yPU")
+for video in pl.videos:
+    video.streams.first().download()
 
-# DOWNLOAD UNICO - INFORMAR LINK DO VIDEO
+yt = YouTube(link)
 
+def show_progress_bar(stream, chunk, file_handle, bytes_remaining):
+         return  # do work
 
-def downloadURL(url):
+yt.register_on_progress_callback(show_progress_bar)
 
-    youtube = pytube.YouTube(url)
+def convert_to_aac(stream, file_handle):
+         return  # do work
 
-    # traz lista de formato disponiveis
-    i = 1
-    for lista in youtube.streams.order_by('resolution'):
-        try:
-            print(lista)
-            i += 1
-        except:
-            pass
+yt.register_on_complete_callback(convert_to_aac)'''
 
-    # Conecta para preparar o video
-    video = youtube.streams.filter(
-        progressive=True, file_extension="mp4").order_by('resolution').last()
-
-    # Titulo do video
-    title = video.title
-    print("Iniciando Download : " + str(title))
-
-    # Path do Download
-    video.download("/home/pinheirocfc/Downloads/")
-
-    print("Download Concluido!")
-
-# DOWNLOAD DE PLAYLIST
+sg.theme('Reddit')
 
 
-def downloadPlaylist(url_playlist):
-    pl = Playlist(url_playlist)
-    i = 1
-    print("Iniciando Download Playlist")
-    for video in pl.videos:
-        video.streams.filter(progressive=True, file_extension="mp4").order_by(
-            'resolution').get_highest_resolution().download("/home/pinheirocfc/Downloads/")
-        try:
-            # Titulo do video
-            title = video.title
-            print("Video " + str(i) + ": " +
-                  str(title) + " concluído com sucesso!")
-            i += 1
-        except:
-            pass
-    print("Iniciando")
+layout = [
+    [sg.Image(r'G:\Dropbox\OHomemnãoparaNunca\DownTube\youtube.png')],
+    [sg.Text('Digite o Link ')],
+    [sg.Input(key='link')],
+    [sg.Text('Pasta de Saida ')],
+    [sg.Input(key='caminho_arquivo'), sg.FolderBrowse(
+        'Procurar Pasta', target='caminho_arquivo', key='caminho')],
+    [sg.Button('Baixar', key='baixar')],
+    [sg.Text('Titulo do video')],
+    [sg.Input(key='titulo')],
+    [sg.Text('Numero de views ')],
+    [sg.Input(key='views')],
+    [sg.Text('Tamanho do Video ')],
+    [sg.Input(key='tamanho')],
+    [sg.Text('Avaliação do video ')],
+    [sg.Input(key='avaliacao')]
+]
+
+janela = sg.Window('DownTube', layout)
 
 
-# chamar funcao para download unico
-downloadURL("https://www.youtube.com/watch?v=DJVPIlDT2zw&list=RDMM&index=2")
-# downloadPlaylist("https://www.youtube.com/watch?v=UrT0zCmsN8c&list=PLsEx1ZKe8IpVY9ltdnBW60FOiPqtiMAMD") #download de playlist, copiar url d playlist
-exit()
+while True:
+    evento, valores = janela.read()
+
+    if evento == sg.WINDOW_CLOSED:
+        janela.close()
+        sys.exit()
+    elif evento == 'baixar':
+        link = valores['link']
+        caminho = valores['caminho']
+        tra_caminho = f'{caminho}'
+        yt = YouTube(link)
+        janela['titulo'].update(yt.title)
+        janela['views'].update(yt.views)
+        janela['tamanho'].update(yt.length)
+        janela['avaliacao'].update(yt.rating)
+        ys = yt.streams.get_highest_resolution()
+        print('Baixando')
+        ys.download(tra_caminho)
+        print('Download Completo!')
